8ebc4cd654d61c62ffc9a59e63d8a7614812649c
Ryad Juliano <ryadjuliano@gmail.com>
2024-02-06 13:57:15 +0700

    new branch for prod

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/spos/';
+$config['base_url'] = 'https://pos.x-onefitness.com/';
 
 /*
 |--------------------------------------------------------------------------
